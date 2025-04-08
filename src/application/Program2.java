c91620abb7d22187fca6512c857d485d5adc8dbe
Junio Note <amiltonsogm@gmail.com>
2025-04-08 09:46:37 -0300

    ajustes finais no código

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,14 +1,11 @@
 package application;
 
-import java.util.Date;
+
 import java.util.List;
 import java.util.Scanner;
-
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
-import model.dao.SellerDao;
 import model.entities.Department;
-import model.entities.Seller;
 
 public class Program2 {
 public static void main(String[] args) {
